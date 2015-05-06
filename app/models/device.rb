6feb7360b729c12b8ac87d8cbfcbbc2a6843b0a7
Rob Richey <robsnothome@gmail.com>
2015-05-06 17:49:00 -0600

    Adds method to ping a device.

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,2 +1,10 @@
 class Device < ActiveRecord::Base
+
+	# This will be used to ping a device to see if it is available. 
+	def ping
+		result = `ping -c 1 #{ip_address}`
+		# Now extract the time that it took for the ping to come back
+		result = result.split("time=").last.split(" ").first
+		return result
+	end
 end
