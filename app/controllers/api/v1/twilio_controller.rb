efae0ae9d3303f9806da39720b8fac49a575aacb
Rob Richey <robsnothome@gmail.com>
2015-09-29 11:50:50 -0600

    Adds some additional debugging and testing stuff.

--- app/controllers/api/v1/twilio_controller.rb
+++ app/controllers/api/v1/twilio_controller.rb
@@ -4,5 +4,34 @@ class Api::V1::TwilioController < ApplicationController
 
   def text_test
     params.inspect
+    # Parameters: 
+    # {"ToCountry"=>"US", 
+    # "ToState"=>"UT", 
+    # "SmsMessageSid"=>"SM5d11339688438ea6840b1ff5cf6b3e33", 
+    # "NumMedia"=>"0", 
+    # "ToCity"=>"PARK CITY", 
+    # "FromZip"=>"84770", 
+    # "SmsSid"=>"SM5d11339688438ea6840b1ff5cf6b3e33", 
+    # "FromState"=>"UT", 
+    # "SmsStatus"=>"received", 
+    # "FromCity"=>"ST GEORGE", 
+    # "Body"=>"testing 5678", 
+    # "FromCountry"=>"US", 
+    # "To"=>"+14352141844", 
+    # "ToZip"=>"84098", 
+    # "NumSegments"=>"1", 
+    # "MessageSid"=>"SM5d11339688438ea6840b1ff5cf6b3e33", 
+    # "AccountSid"=>"ACfb03eaaab63e59c4a90d5bf07b5f6f70", 
+    # "From"=>"+14356691878", 
+    # "ApiVersion"=>"2010-04-01"}
+    from = params[:From]
+
+  end
+  def text_engine
+    # This method will need to check the customer database of TQMS for the customer with a phone number that is the from number.
+    if params[:body].contains?("status")
+      render "status"
+    end
+
   end
 end
